346dbff73d6f1017459f7caf8fb51cafd2115470
EliveltonF <diogoelivelton18@gmail.com>
2021-09-06 14:31:36 -0300

    interface

---
+++ script.js
@@ -0,0 +1,17 @@
+function mudarEstado(el) {
+
+    var display = document.getElementById(el);
+        if(display.style.display == "none")
+           display.style.display = 'block';
+        else
+           display.style.display = 'none';   
+}
+
+/*
+div:active > img {
+    transform: rotate(180deg);
+}
+
+        
+
+           */
